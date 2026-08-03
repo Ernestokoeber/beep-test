e42c7386d16b7ed7d50b4e998dbbcc3fb6c5c596
Ernesto <ekoeber@protonmail.com>
2026-08-03 21:16:05 +0200

    feat: add Play Designer module entry

---
+++ js/play-designer/main.js
@@ -0,0 +1,5 @@
+import { injectStyles } from './styles.js';
+import { mountEditor as editor } from './editor.js';
+import { mountPlayer as player } from './viewer.js';
+export function mountEditor(target){injectStyles();return editor(target);}
+export function mountPlayer(target){injectStyles();return player(target);}
